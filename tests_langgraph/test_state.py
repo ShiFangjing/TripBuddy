07eae1be63b8d1ee0902799d16308bbeb8b07f9c
ShiFangjing <41350291+ShiFangjing@users.noreply.github.com>
2026-05-16 17:50:07 +0800

    Delete tests_langgraph directory

--- tests_langgraph/test_state.py
+++
@@ -1,7 +0,0 @@
-from app.state import REQUIRED_KEYS, make_initial_state
-
-
-def test_make_initial_state_has_required_keys():
-    state = make_initial_state(user_id="u1", session_id="s1", user_input="hello")
-    for key in REQUIRED_KEYS:
-        assert key in state
